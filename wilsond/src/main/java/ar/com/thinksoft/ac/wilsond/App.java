98cd529a59162163fc9294e8514079a8e8d6ddda
Adriel Paredes <adriel.paredes@gmail.com>
2011-04-21 15:42:05 -0300

    Proyecto inicial del server

---
+++ wilsond/src/main/java/ar/com/thinksoft/ac/wilsond/App.java
@@ -0,0 +1,13 @@
+package ar.com.thinksoft.ac.wilsond;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
