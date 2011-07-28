2ff0f7b6689520744b5094dbf6a6d3d5f9d8c05e
Adriel Paredes <adriel.paredes@gmail.com>
2011-07-28 10:55:45 -0300

    Issue #29 : Se modifica la pagina base para que todas las paginas que exntiendan de ella puedan tener el formato general de la pagina web

--- webac/src/main/java/ar/com/thinksoft/ac/webac/web/base/BasePage.java
+++ webac/src/main/java/ar/com/thinksoft/ac/webac/web/base/BasePage.java
@@ -1,11 +1,13 @@
 package ar.com.thinksoft.ac.webac.web.base;
 
+import org.apache.wicket.markup.html.CSSPackageResource;
+
 import ar.com.thinksoft.ac.webac.seguridad.Permitible;
 
 public abstract class BasePage extends Permitible {
 
 	public BasePage() {
-
+		add(CSSPackageResource.getHeaderContribution(BasePage.class,"BasePage.css"));
 	}
 	
 	private void createHeader(){
