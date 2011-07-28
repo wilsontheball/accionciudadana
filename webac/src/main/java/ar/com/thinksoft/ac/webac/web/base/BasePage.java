7d52237b100aa083ece1e7f60e6432a030b8ef1f
Adriel Paredes <adriel.paredes@gmail.com>
2011-07-28 09:30:27 -0300

    Issue #29 : Se creo una pagina base de la cual extender para hacer el resto de las paginas. Falta darle el estilo y hacer el menu.

---
+++ webac/src/main/java/ar/com/thinksoft/ac/webac/web/base/BasePage.java
@@ -0,0 +1,15 @@
+package ar.com.thinksoft.ac.webac.web.base;
+
+import ar.com.thinksoft.ac.webac.seguridad.Permitible;
+
+public abstract class BasePage extends Permitible {
+
+	public BasePage() {
+
+	}
+	
+	private void createHeader(){
+		
+	}
+
+}
