9ee993d6d5a4b714aec2cd43491dddae148cfe1f
Adriel Paredes <adriel.paredes@gmail.com>
2011-07-28 21:00:28 -0300

    Issue #29 : Se agrega el login y un pseudoenganche con el menu

--- webac/src/main/java/ar/com/thinksoft/ac/webac/web/base/BasePage.java
+++ webac/src/main/java/ar/com/thinksoft/ac/webac/web/base/BasePage.java
@@ -1,17 +1,27 @@
 package ar.com.thinksoft.ac.webac.web.base;
 
 import org.apache.wicket.markup.html.CSSPackageResource;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.link.IPageLink;
 
 import ar.com.thinksoft.ac.webac.seguridad.Permitible;
+import ar.com.thinksoft.ac.webac.web.login.Login;
 
 public abstract class BasePage extends Permitible {
 
 	public BasePage() {
+		
 		add(CSSPackageResource.getHeaderContribution(BasePage.class,"BasePage.css"));
+		
+		
+		this.appendLinks();
+		
+		
+		
 	}
 	
-	private void createHeader(){
-		
+	private void appendLinks(){
+		add(new BookmarkablePageLink<IPageLink>("loginLink", Login.class));
 	}
 
 }
---
+++ webac/src/main/java/ar/com/thinksoft/ac/webac/web/login/Login.java
@@ -0,0 +1,27 @@
+package ar.com.thinksoft.ac.webac.web.login;
+
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+
+import ar.com.thinksoft.ac.intac.IPermiso;
+import ar.com.thinksoft.ac.webac.web.base.BasePage;
+
+public class Login extends BasePage {
+
+	@Override
+	public IPermiso getPermisoNecesario() {
+		return new LoginPermiso();
+	}
+
+	public Login(final PageParameters parameters) {
+
+		add( new Button("accederButton"));
+		add( new Button("registrarButton"));
+		
+		
+	}
+	
+
+
+}
