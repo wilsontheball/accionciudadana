424135b119f4d6f70f4eac3608fa30b27bab11f3
Matias Tarrio Pages <matias.tarriopages@gmail.com>
2011-08-07 19:34:55 -0300

    Modificaciones en base y se agrega pagina de Alta de Reclamo

--- webac/src/main/java/ar/com/thinksoft/ac/webac/web/base/BasePage.java
+++ webac/src/main/java/ar/com/thinksoft/ac/webac/web/base/BasePage.java
@@ -1,18 +1,22 @@
 package ar.com.thinksoft.ac.webac.web.base;
 
 import org.apache.wicket.markup.html.CSSPackageResource;
+import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.IPageLink;
+import org.apache.wicket.resource.ContextRelativeResource;
 
 import ar.com.thinksoft.ac.webac.HomePage;
 import ar.com.thinksoft.ac.webac.seguridad.Permitible;
 import ar.com.thinksoft.ac.webac.web.login.LoginPage;
+import ar.com.thinksoft.ac.webac.web.reclamo.AltaReclamoPage;
 
 public abstract class BasePage extends Permitible {
 
 	public BasePage() {
 		
 		add(CSSPackageResource.getHeaderContribution(BasePage.class,"../css/BasePage.css"));
+		add(new Image("thinksoft", new ContextRelativeResource("thinksoft.jpg")));
 		
 		this.appendLinks();
 		
@@ -23,6 +27,7 @@ public abstract class BasePage extends Permitible {
 	private void appendLinks(){
 		add(new BookmarkablePageLink<IPageLink>("loginLink", LoginPage.class));
 		add(new BookmarkablePageLink<IPageLink>("homeLink", HomePage.class));
+		add(new BookmarkablePageLink<IPageLink>("altaReclamoLink", AltaReclamoPage.class));
 	}
 
 }
