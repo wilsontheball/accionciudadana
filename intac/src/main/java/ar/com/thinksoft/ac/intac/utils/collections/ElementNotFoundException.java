a8a95ac2a73ee4879a79c9f519a0796c9f7b402e
Adriel Paredes <adriel.paredes@gmail.com>
2011-08-18 16:30:57 -0300

    Se agrega coleccion util para el proyecto

---
+++ intac/src/main/java/ar/com/thinksoft/ac/intac/utils/collections/ElementNotFoundException.java
@@ -0,0 +1,10 @@
+package ar.com.thinksoft.ac.intac.utils.collections;
+
+public class ElementNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1491062033897295020L;
+
+}
