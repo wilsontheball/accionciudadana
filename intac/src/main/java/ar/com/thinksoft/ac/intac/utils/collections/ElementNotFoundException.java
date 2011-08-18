371c2190220ce71d93699c4895e8df4912cc659c
Adriel Paredes <adriel.paredes@gmail.com>
2011-08-18 16:31:35 -0300

    Se mejora el mensaje de error

--- intac/src/main/java/ar/com/thinksoft/ac/intac/utils/collections/ElementNotFoundException.java
+++ intac/src/main/java/ar/com/thinksoft/ac/intac/utils/collections/ElementNotFoundException.java
@@ -6,5 +6,9 @@ public class ElementNotFoundException extends RuntimeException {
 	 * 
 	 */
 	private static final long serialVersionUID = 1491062033897295020L;
+	
+	public ElementNotFoundException() {
+		super("El elemento especificado no se encuentra en la coleccion");
+	}
 
 }
