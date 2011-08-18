010101b08bbb36910ea4582191252ddeacde881d
Adriel Paredes <adriel.paredes@gmail.com>
2011-08-18 12:25:13 -0300

    Issue #29 : Se agrego el panel para los errores, se agregaron validadores, falta mejorar los validadores y un poco el codigo

--- webac/src/main/java/ar/com/thinksoft/ac/webac/web/registro/RegistroPage.java
+++ webac/src/main/java/ar/com/thinksoft/ac/webac/web/registro/RegistroPage.java
@@ -1,6 +1,7 @@
 package ar.com.thinksoft.ac.webac.web.registro;
 
 import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 
 import ar.com.thinksoft.ac.intac.IPermiso;
 import ar.com.thinksoft.ac.webac.web.base.BasePage;
@@ -14,6 +15,7 @@ public class RegistroPage extends BasePage {
 	
 	public RegistroPage(final PageParameters parameters) {
 		add(new RegistroForm("registroForm"));
+		add(new FeedbackPanel("feedback"));
 	}
 	
 	
