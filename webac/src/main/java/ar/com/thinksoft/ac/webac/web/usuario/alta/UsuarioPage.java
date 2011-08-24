c0d98d1aa2140d428793e5e4dce02cf8fe3acfb9
Adriel Paredes <adriel.paredes@gmail.com>
2011-08-24 14:07:21 -0300

    Issue #29 : Se agrego el esqueleto del filtro.

--- webac/src/main/java/ar/com/thinksoft/ac/webac/web/usuario/alta/UsuarioPage.java
+++ webac/src/main/java/ar/com/thinksoft/ac/webac/web/usuario/alta/UsuarioPage.java
@@ -1,20 +1,23 @@
 package ar.com.thinksoft.ac.webac.web.usuario.alta;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
 import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.CSSPackageResource;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.repeater.data.ListDataProvider;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
-
 import ar.com.thinksoft.ac.intac.IPermiso;
 import ar.com.thinksoft.ac.intac.IUsuario;
 import ar.com.thinksoft.ac.webac.predicates.registro.PredicateTodosLosUsuarios;
 import ar.com.thinksoft.ac.webac.repository.Repository;
 import ar.com.thinksoft.ac.webac.web.base.BasePage;
 import ar.com.thinksoft.ac.webac.web.reclamo.busquedaReclamo.BusquedaReclamoPage;
-
 import com.inmethod.grid.DataProviderAdapter;
 import com.inmethod.grid.IGridColumn;
 import com.inmethod.grid.column.PropertyColumn;
@@ -28,21 +31,26 @@ public class UsuarioPage extends BasePage {
 	}
 
 	public UsuarioPage() {
-		
-		add(CSSPackageResource.getHeaderContribution(BusquedaReclamoPage.class,"../../css/UsuarioPage.css"));
-		
+
+		add(CSSPackageResource.getHeaderContribution(BusquedaReclamoPage.class, "../../css/UsuarioPage.css"));
+		this.add(new Label("labelNombre","Nombre"));
+		this.add(new TextField<String>("campoBusquedaNombre"));
+		this.add(new TextField<String>("campoBusquedaApellido"));
+		this.add(new Button("botonBuscar"));
+		this.add(new Button("botonNuevo"));
 		this.add(this.createTablaUsuarios("grid"));
 
 	}
+	
 
 	private Component createTablaUsuarios(String gridName) {
 
 		ListDataProvider<IUsuario> dataProvider = new ListDataProvider<IUsuario>(
-				this.getTodosLosUsuarios());
+			this.getTodosLosUsuarios());
 		List<IGridColumn> columnas = this.crearColumnas();
 
 		DataGrid grid = new DataGrid(gridName, new DataProviderAdapter(
-				dataProvider), columnas);
+			dataProvider), columnas);
 
 		grid.setRowsPerPage(10);
 		grid.setClickRowToSelect(true);
@@ -61,16 +69,16 @@ public class UsuarioPage extends BasePage {
 		List<IGridColumn> columnas = new ArrayList<IGridColumn>();
 
 		columnas.add(new PropertyColumn("apellido", new Model<String>(
-				"Apellido"), "apellido"));
+			"Apellido"), "apellido"));
 		columnas.add(new PropertyColumn("nombre", new Model<String>("Nombre"),
-				"nombre"));
+			"nombre"));
 		columnas.add(new PropertyColumn("nombreUsuario", new Model<String>(
-				"Nombre de Usuario"), "nombreUsuario"));
+			"Nombre de Usuario"), "nombreUsuario"));
 		columnas.add(new PropertyColumn("dni", new Model<String>("DNI"), "dni"));
 		columnas.add(new PropertyColumn("mail", new Model<String>("E-Mail"),
-				"mail"));
+			"mail"));
 		columnas.add(new PropertyColumn("telefono", new Model<String>(
-				"Telefono"), "telefono"));
+			"Telefono"), "telefono"));
 
 		return columnas;
 	}
