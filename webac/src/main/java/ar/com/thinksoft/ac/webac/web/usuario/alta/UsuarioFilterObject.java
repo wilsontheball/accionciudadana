2c3a83d8c9ce5214223f166580a12dedeb14478d
Adriel Paredes <adriel.paredes@gmail.com>
2011-08-28 16:30:48 -0300

    Issue #29 : se agregan los forms faltantnes en el commit anterior

---
+++ webac/src/main/java/ar/com/thinksoft/ac/webac/web/usuario/alta/UsuarioFilterObject.java
@@ -0,0 +1,35 @@
+package ar.com.thinksoft.ac.webac.web.usuario.alta;
+
+public class UsuarioFilterObject {
+
+	private String nombre = "";
+	private String apellido = "";
+	private String nombreUsuario = "";
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+	
+
+}
