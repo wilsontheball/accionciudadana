0ef328c41fc130664dd060b8b3905cbb8b0ba448
Matias Tarrio Pages <matias.tarriopages@gmail.com>
2011-09-27 10:13:17 -0300

    Posible problema de commit...

--- webac/src/main/java/ar/com/thinksoft/ac/webac/web/js/detalleReclamo.js
+++ webac/src/main/java/ar/com/thinksoft/ac/webac/web/js/detalleReclamo.js
@@ -1,20 +1,13 @@
 $(document).ready(function() {
-	var ruta = $('.rutaImagen')[0].textContent;
-	if(ruta == undefined || ruta == '' || ruta == null){
-		$('.contenedorImagen').css('display','none');
-	}else{
-		setTimeout(function(){
-			$('.imagen').attr('src',"/tempImages/" + ruta);
-			$('.imagen').css('heigth','auto');
-			$('.imagen').css('width','auto');
-		}, 3 * 1000);
-	}
-
+	setTimeout(function(){
+		var ruta = $('.rutaImagen')[0].textContent;
+		$('.imagen').attr('src',"/tempImages/" + ruta);
+		$('.imagen').css('heigth','auto');
+		$('.imagen').css('width','auto');
+	}, 3 * 1000);
+	
 	if($('.numAsociados')[0].textContent == ""){
 		$('.asociados').css('display','none');
 	}
 	
-	var latitud = $('.latitudIncidente')[0].textContent;
-	var longitud = $('.longitudIncidente')[0].textContent;
-	$('.map').attr('src',"http://maps.google.com/maps/api/staticmap?zoom=14&size=512x512&maptype=roadmap&markers=color:blue|"+ latitud +"," + longitud + "&sensor=false");
 });
