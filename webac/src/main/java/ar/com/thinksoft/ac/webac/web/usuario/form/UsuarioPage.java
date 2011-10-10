f6ac5c649dfbb2c41a1a81dad737790a3735f75c
Adriel Paredes <adriel.paredes@gmail.com>
2011-10-09 21:00:36 -0300

    Framework de auth funcional

--- webac/src/main/java/ar/com/thinksoft/ac/webac/web/usuario/form/UsuarioPage.java
+++ webac/src/main/java/ar/com/thinksoft/ac/webac/web/usuario/form/UsuarioPage.java
@@ -1,14 +1,16 @@
 package ar.com.thinksoft.ac.webac.web.usuario.form;
 
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.markup.html.CSSPackageResource;
-
 import ar.com.thinksoft.ac.webac.web.base.BasePage;
 import ar.com.thinksoft.ac.webac.web.reclamo.busquedaReclamo.BusquedaReclamoPage;
 
 
+@AuthorizeInstantiation("BLEH")
 public class UsuarioPage extends BasePage {
 
-	public UsuarioPage() {
+	public UsuarioPage(final PageParameters parameters) {
 
 		add(CSSPackageResource.getHeaderContribution(BusquedaReclamoPage.class, "../../css/UsuarioPage.css"));
 
