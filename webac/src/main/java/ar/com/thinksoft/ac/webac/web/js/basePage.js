e3d81e4cacfdd7b6f82b3b4e4a16a098979b48e0
Matias Tarrio Pages <matias.tarriopages@gmail.com>
2011-11-04 14:27:14 -0300

    Se saca validacion de ingreso con celular, se actualiza andrac a 1.4

--- webac/src/main/java/ar/com/thinksoft/ac/webac/web/js/basePage.js
+++ webac/src/main/java/ar/com/thinksoft/ac/webac/web/js/basePage.js
@@ -1,14 +1,3 @@
-$(document).ready(function() {
-	
-	if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) ||
-			 navigator.userAgent.match(/iPhone/i) ||  navigator.userAgent.match(/iPod/i))
-	{
-		 alert("El sistema web no es compatible con su dispositivo móvil.");
-		 
-	}
-	
-});
-
 /*
  * FUNCIONES DE VALIDACION
  */
